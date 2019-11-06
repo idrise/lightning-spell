951537866c725c4addb3061705de5b7368413aa9
LiSmi <41838831+LiSmi@users.noreply.github.com>
2019-11-06 10:16:23 +0000

    add test

--- lib/binding.spec.ts
+++ lib/binding.spec.ts
@@ -23,4 +23,26 @@ describe('LightningSpell', () => {
         expect(lightningSpell.isCorrectlySpelled('united state')).toEqual(true)
         expect(lightningSpell.isCorrectlySpelled('united stte')).toEqual(false)
     });
+
+    it('should work with 0f', () => {
+        const lightningSpell = new LightningSpell("test");
+        
+        lightningSpell.createDictionaryEntry('of',1);
+        lightningSpell.createDictionaryEntry('if',1);
+        lightningSpell.createDictionaryEntry('0d',1);
+        lightningSpell.createDictionaryEntry('0s',1);
+        lightningSpell.createDictionaryEntry('off',1);
+        lightningSpell.createDictionaryEntry('raf',1);
+        lightningSpell.createDictionaryEntry('orf',1);
+        lightningSpell.createDictionaryEntry('oof',1);
+
+        expect(lightningSpell.getSuggestions('0f', {maxResults: 8, distance: 2})).toEqual(["of",
+        "if",
+        "0d",
+        "0s",
+        "off",
+        "raf",
+        "orf",
+        "oof"])
+    })
 });
